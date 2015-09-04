13e2e49f04ace54412294b36f1dbac8b8110e3a3
Kiran Kumar <kiran.kumaravelu@gmail.com>
2015-09-04 12:28:27 +1000

    Added Test Methods for testing.

--- StraxImage/StraxImage.App.Services.Tests/HomeControllerTest.cs
+++ StraxImage/StraxImage.App.Services.Tests/HomeControllerTest.cs
@@ -9,6 +9,37 @@ namespace StraxImage.App.Services.Tests
         [TestMethod]
         public void TestMethod1()
         {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void TestMethod5()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void TestMethod6()
+        {
+            Assert.IsTrue(true);
         }
     }
 }
